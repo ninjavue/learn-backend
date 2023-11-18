0461a89375fcf92eb5426c703ccfc1e0efbd4f10
yoldoshov <umrzoqdev@gmail.com>
2023-11-18 16:53:34 +0500

    server config pro

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ const categoryRouter = require('./router/category')
 const productRouter = require('./router/product')
 const authROuter = require('./router/auth')
 
-const keys = require('./keys/dev')
+const keys = require('./keys/pro')
 
 
 app.use(function (req, res, next) {
